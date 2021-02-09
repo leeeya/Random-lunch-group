fbd8bfa4d4d15f10b1d823d9855750e178414d90
YOONA LEE <lya1290@gmail.com>
2021-02-10 01:09:32 +0900

    feat: add error route for server

--- server/main.js
+++ server/main.js
@@ -8,8 +8,28 @@ const peopleRouter = require('./routes/people.route');
 
 app.use('/api', peopleRouter);
 
-app.listen(2018, () => {
-  console.log('Lunch app is listening on port 2018!') // eslint-disable-line no-console
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  app.use((err, req, res, next) => {
+    console.log(err);
+
+    if (process.env.NODE_ENV === 'production') {
+      if (err instanceof mongoose.Error) err = createError(500);
+      err.stack = null;
+    }
+
+    res.status(err.status || 500);
+    res.json(err);
+  });
+});
+
+app.listen(2021, () => {
+  console.log('Lunch app is listening on port 2021!') // eslint-disable-line no-console
 });
 
 module.exports = app;
